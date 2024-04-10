89a05f97883aabdb5ebec6c7297b35cf0171112c
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-04-10 20:28:17 +0700

    Add repository for role and user

---
+++ backend/grabwebapp/src/main/java/com/grab/grabwebapp/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.grab.grabwebapp.repository;
+
+import com.grab.grabwebapp.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}

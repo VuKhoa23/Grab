574ca3e8c82aec3aca880cdb277fc6fc859797ef
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-04-17 16:41:44 +0700

    Fix request header

--- backend/grabwebapp/src/main/java/com/grab/grabwebapp/security/JwtAuthFilter.java
+++ backend/grabwebapp/src/main/java/com/grab/grabwebapp/security/JwtAuthFilter.java
@@ -35,7 +35,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private String getJwtFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer: ")){
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
             return bearerToken.substring(7, bearerToken.length());
         }
         return null;
